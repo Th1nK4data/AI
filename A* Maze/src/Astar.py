9cc07d0b5de8abcb6668ed4be2b406c3dc65868b
Yuhua Ni <yuhuani50@gmail.com>
2016-02-11 16:36:49 -0500

    slight additions to Astar

--- A* Maze/src/Astar.py
+++ A* Maze/src/Astar.py
@@ -4,6 +4,14 @@ class Astar:
     def __init__(self, size):
         self.size = size
         self.grid = np.array([[0 for i in range(self.size)] for i in range(self.size)])
+        self.H = np.array([[0 for i in range(self.size)] for i in range(self.size)])
+        self.G = 10
+        self.queue = []
+        self.initialize()
         
     def solve(self):
-        
+        pass
+    def initialize(self):
+        # finalize grid
+        # calculate H values based on finalized grid
+        pass
